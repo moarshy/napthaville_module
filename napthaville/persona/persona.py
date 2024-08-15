3f785c4cb1cd640b43fd499ec6e5eaa041ebcde6
moarshy <marshath@gmail.com>
2024-08-15 13:38:43 +0800

    more updates

--- napthaville/persona/persona.py
+++ napthaville/persona/persona.py
@@ -31,13 +31,13 @@ class Persona:
         # If there is already memory in folder_mem_saved, we load that. Otherwise,
         # we create new memory instances.
         # <s_mem> is the persona's spatial memory.
-        f_s_mem_saved = f"{folder_mem_saved}/bootstrap_memory/spatial_memory.json"
+        f_s_mem_saved = f"{folder_mem_saved}/spatial_memory.json"
         self.s_mem = MemoryTree(f_s_mem_saved)
         # <s_mem> is the persona's associative memory.
-        f_a_mem_saved = f"{folder_mem_saved}/bootstrap_memory/associative_memory"
+        f_a_mem_saved = f"{folder_mem_saved}/associative_memory"
         self.a_mem = AssociativeMemory(f_a_mem_saved)
         # <scratch> is the persona's scratch (short term memory) space.
-        scratch_saved = f"{folder_mem_saved}/bootstrap_memory/scratch.json"
+        scratch_saved = f"{folder_mem_saved}/scratch.json"
         self.scratch = Scratch(scratch_saved)
 
     def save(self, save_folder):
